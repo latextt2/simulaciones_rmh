0bceab2bea08c35e6b986909b47253b281672417
Manuel Limones <limonesu.me@gmail.com>
2017-08-01 14:08:48 -0500

    Remove unecessary files

--- simulaciones_dinamicas/06_model_complete/calc_discrete_controller_gains.m
+++
@@ -1,8 +0,0 @@
-function [Kd,Kdo] = calc_discrete_controller_gains(A,B,C,P,Po,Ts)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-[Ad,Bd,Cd] = get_discrete_linear_model(A,B,C,Ts);
-Pd = get_discrete_poles(P,Ts);
-Pdo = get_discrete_poles(Po,Ts);
-[Kd,Kdo] = calc_linear_controller_gains(Ad,Bd,Cd,Pd,Pdo);
-end
