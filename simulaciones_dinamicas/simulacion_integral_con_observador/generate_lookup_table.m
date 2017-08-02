794f99ea6aca8684d990c0118480d62811db247f
Manuel Limones <limonesu.me@gmail.com>
2017-08-01 19:25:54 -0500

    Agregar control calendarizado integral continuo

--- simulaciones_dinamicas/simulacion_integral_con_observador/generate_lookup_table.m
+++ simulaciones_dinamicas/simulacion_integral_con_observador/generate_lookup_table.m
@@ -5,7 +5,7 @@ P = -((1:10)*4 + 300);
 Po = -((1:10)*4 + 600); 
 Pi = [P,-((1:5)+30)];
 Ts = 0.5e-3;
-ws_lt = -10000 : 1000 :10000;
+ws_lt = -30000 : 2500 :30000;
 
 generate_non_linear_model();
 [xp_nl,y_nl] = get_symbolic_model('control');
@@ -35,3 +35,12 @@ Kd_lt(:,:,n) = Kd;
 Kdo_lt(:,:,n) = Kdo;
 Kdi_lt(:,:,n) = Kdi;
 end
+
+ws = 0;
+[A,B,C] = get_linear_model(xp_nl,y_nl,ws);
+[K, Ko] = calc_linear_controller_gains(A,B,C,P,Po);
+Ki = calc_integral_controller_gains(A,B,C,Pi);
+
+[Ad,Bd,Cd] = get_discrete_linear_model(A,B,C,Ts);
+[Kd,Kdo] = calc_discrete_controller_gains(A,B,C,P,Po,Ts);
+Kdi = calc_discrete_integral_controller_gains(A,B,C,Pi,Ts);
